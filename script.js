a89cb29ca7cb6ac6d2fa7313cd8b07172c5b9930
Ritesh-Aggarwal <57039447+Ritesh-Aggarwal@users.noreply.github.com>
2021-05-16 22:32:27 +0530

    minor changes

--- script.js
+++ script.js
@@ -21,19 +21,11 @@ function changewinner(num){
   var num2 = dicethrow(); 
 
   img1.setAttribute("src",pathgen(num1));
-  console.log(img1);
   img2.setAttribute("src",pathgen(num2));
-  console.log(img2);
 
-if(num1 > num2){
+if(num1 > num2)
     changewinner(1);
-    p1++;round++;
-}
-else if(num2 > num1){
+else if(num2 > num1)
     changewinner(2);
-    p2++;
-}
-else{
+else
     changewinner(3);
-    round++;
-}
