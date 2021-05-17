f8db8662e08624ebd546b09c8629824171991739
Ritesh-Aggarwal <57039447+Ritesh-Aggarwal@users.noreply.github.com>
2021-05-17 15:17:25 +0530

    Add buttons

--- script.js
+++ script.js
@@ -1,31 +1,68 @@
-function pathgen(num) {
+var img1 = document.querySelector("#one");
+var img2 = document.querySelector("#two");
+
+var p1 = 0;
+var p2 = 0;
+var round = 0;
+
+function updateCount(){
+    document.querySelector("#p1").innerHTML = p1;
+    document.querySelector("#p2").innerHTML = p2;
+    document.querySelector("#round").innerHTML = round;
+    if(p1 > p2)
+        document.querySelector("#total").innerHTML = "Player 1";
+    else if(p2 > p1)
+        document.querySelector("#total").innerHTML = "Player 2";
+    else if(p1==p2)
+        document.querySelector("#total").innerHTML = "Draw";
+}
+
+function pathGen(num) {
     return "images/dice" + num + ".png";
 }
 
-function dicethrow(){
+function diceThrow(){
     return Math.round(Math.random()*5) + 1;
 }
 
-function changewinner(num){
+function changeWinner(num){
     if(num == 3)
         document.querySelector(".top").innerHTML = "Draw!"
     else
     document.querySelector(".top").innerHTML = "Player " + num + " wins!!"
 }
+function rollDice(){
 
-  var img1 = document.querySelector("#one");
+  var num1 = diceThrow();
+  var num2 = diceThrow(); 
 
-  var img2 = document.querySelector("#two");
+  img1.setAttribute("src",pathGen(num1));
+  img2.setAttribute("src",pathGen(num2));
 
-  var num1 = dicethrow();
-  var num2 = dicethrow(); 
-
-  img1.setAttribute("src",pathgen(num1));
-  img2.setAttribute("src",pathgen(num2));
-
-if(num1 > num2)
-    changewinner(1);
-else if(num2 > num1)
-    changewinner(2);
+if(num1 > num2){
+    changeWinner(1);
+    p1++;
+}
+else if(num2 > num1){
+    changeWinner(2);
+    p2++;
+}
 else
-    changewinner(3);
+    changeWinner(3);
+
+round++;
+
+updateCount();
+}
+
+function reset(){
+    img1.setAttribute("src",pathGen(1));
+    img2.setAttribute("src",pathGen(2));
+    document.querySelector(".top").innerHTML = "Dice Game";
+    round = 0;
+    p1 = 0;
+    p2 = 0;
+    updateCount();
+}
+document.querySelector("#rollDice").addEventListener("click",rollDice);
+document.querySelector("#Reset").addEventListener("click",reset);
